a7a0dd673c9b79bcb4af0e5ae68ae12dc0e25395
Lorenz Nenntwich <lorenz@e-dope.com>
2021-11-16 16:42:18 +0100

    docs: update graphView.js comments
    #34

--- webview-resources/graphView.js
+++ webview-resources/graphView.js
@@ -1,4 +1,8 @@
 // @ts-check
+// TODO: Try to implement a pre-build/watch (transpiler) script to be run before the actual watcher/build
+// to empower such Webview specific js scripts with full TypeScript support, after the prototype phase.
+// For now we just go with the js solution to speed up the initial development,
+// inherited types (e.g. for cytoscape) can still be checked via ts-check...
 
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
@@ -6,7 +10,8 @@
   // @ts-ignore
   const vscode = acquireVsCodeApi();
 
-  const testData = [
+  const testData = [ // The elements of the graph, for complete notation see: https://js.cytoscape.org/#notation/elements-json
+    // Nodes
     {
       data: { id: 'a' }
     },
@@ -28,6 +33,7 @@
     {
       data: { id: 'c-1' }
     },
+    // Edges
     {
       data: { id: 'ab', source: 'a', target: 'b' }
     },
@@ -49,9 +55,9 @@
   ];
 
   const initialState = {
-    container: document.getElementById('cy'), // container to render in
+    container: document.getElementById('cy'), // Container to render in
     elements: testData,
-    style: [ // the stylesheet for the graph, for all options see https://js.cytoscape.org/#style
+    style: [ // The stylesheet for the graph, for all options see https://js.cytoscape.org/#style
       {
         selector: 'node',
         style: {
@@ -80,27 +86,27 @@
         }
       }
     ],
-    layout: { // the initial layout, for built-in layouts/algorithms see: https://js.cytoscape.org/#layouts
+    layout: { // The initial layout, for built-in layouts/algorithms see: https://js.cytoscape.org/#layouts
       name: 'breadthfirst',
 
-      fit: true, // whether to fit the viewport to the graph
-      directed: true, // whether the tree is directed downwards (or edges can point in any direction if false)
-      padding: 20, // padding on fit
-      circle: false, // put depths in concentric circles if true, put depths top down if false
-      grid: false, // whether to create an even grid into which the DAG is placed (circle:false only)
-      spacingFactor: 1.2, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
+      fit: true, // Whether to fit the viewport to the graph
+      directed: true, // Whether the tree is directed downwards (or edges can point in any direction if false)
+      padding: 20, // Padding on fit
+      circle: false, // Put depths in concentric circles if true, put depths top down if false
+      grid: false, // Whether to create an even grid into which the DAG is placed (circle:false only)
+      spacingFactor: 1.2, // Positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
       boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-      avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
+      avoidOverlap: true, // Prevents node overlap, may overflow boundingBox if not enough space
       nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
-      roots: undefined, // the roots of the trees
-      maximal: false, // whether to shift nodes down their natural BFS depths in order to avoid upwards edges (DAGS only)
-      animate: false, // whether to transition the node positions
-      animationDuration: 500, // duration of animation in ms if enabled
-      animationEasing: undefined, // easing of animation if enabled,
-      animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-      ready: undefined, // callback on layoutready
-      stop: undefined, // callback on layoutstop
-      transform: function ( node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts
+      roots: undefined, // The roots of the trees
+      maximal: false, // Whether to shift nodes down their natural BFS depths in order to avoid upwards edges (DAGS only)
+      animate: false, // Whether to transition the node positions
+      animationDuration: 500, // Duration of animation in ms if enabled
+      animationEasing: undefined, // Easing of animation if enabled,
+      animateFilter: function ( node, i ){ return true; }, // A function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+      ready: undefined, // Callback on layoutready
+      stop: undefined, // Callback on layoutstop
+      transform: function ( node, position ){ return position; } // Transform a given node position. Useful for changing flow direction in discrete layouts
     }
   };
 
@@ -123,8 +129,9 @@
   // Re-apply the layout (will be used to trigger layout reset via the UI later)
   // cy.layout(graphData.layout).run();
 
-  // Catch any updates on render change for now,
-  // this has to be replaced later with specific events to keep state changes performant
+  // Catch any updates on render change for now
+  // TODO: This has to be replaced later with specific events to keep state changes/mutations performant:
+  // e.g. only pass specific element property updates on mouse release event of a specific element selector.
   cy.on('render', () => {
     // Export changes back to vscode state
     vscode.setState(cy.json());
